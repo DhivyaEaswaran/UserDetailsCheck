db93adde87cce97bdebdd81137b8a0f827917203
DhivyaEaswaran <dhivyamathi@gmail.com>
2023-09-27 22:20:04 +0530

    updated

--- task-one/src/App.js
+++ task-one/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 let userdata ={
   name : "Dhivya",
   imgurl:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Desktop_computer_clipart_-_Yellow_theme.svg/2560px-Desktop_computer_clipart_-_Yellow_theme.svg.png",
-  age: '28'
+  age: ''
 }
 
 function Truevalue(){
   return(<div>
     <h1>Welcome:{userdata.name}</h1>
     <img src ={userdata.imgurl} alt={'photo of the user: '+ userdata.name} width={"200px"}/>
-    <p>The age of the user is {userdata.age}</p>
+    <p>user age: {userdata.age}</p>
   </div>
 
   );
@@ -19,8 +19,8 @@ function Falsyvalue(){
   return(
     <div>
       <h1>Welcome user...</h1>
-      <img src=" https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png" alt='' width={"150px"}/>
-      <p>Age unknown</p>
+      <img src=" https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png" alt="" width={"200px"}/>     
+      <p>Age:unknown</p>
     </div>
 
   );
